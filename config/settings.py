bc8ac15088edaadeb1f35ff87d461f60d7249b96
Israel Colín <israaacolin@gmail.com>
2024-02-23 12:40:23 -0600

    Host BD

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'site',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '192.168.41.101',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
