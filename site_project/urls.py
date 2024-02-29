6b373827dc3025b93ecf4b9358f40b3d413ca2ab
Israel Colín <israaacolin@gmail.com>
2024-02-29 08:58:12 -0600

    CRUD empresa

--- site_project/urls.py
+++ site_project/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('empresa/', views.index_empresa, name='IndexEmpresa'),
     path('empresa/insert_update', views.alta_empresa, name='AltaEmpresa'),
     path('empresa/insert_update/<int:id>',
-         views.alta_empresa, name='AltaEmpresa'),
+         views.alta_empresa, name='EditarEmpresa'),
+    path('empresa/delete/<int:id>', views.eliminar_empresa, name='EliminarEmpresa')
 ]
--- site_project/views.py
+++ site_project/views.py
@@ -18,21 +18,33 @@ def index_empresa(request):
 def alta_empresa(request, id=None):
     mensaje = None
     form = EmpresaForm()
+    edit = False
 
     if id:
         empresa = Empresa.objects.get(pk=id)
+        form = EmpresaForm(instance=empresa)
+        edit = True
 
     if request.method == 'POST':
-        form = EmpresaForm(request.POST)
+
+        if id:
+            form = EmpresaForm(request.POST, instance=empresa)
+        else:
+            form = EmpresaForm(request.POST)
 
         if form.is_valid():
             form.save()
-
             return redirect('IndexEmpresa')
         else:
             mensaje = 'Formulario invalido'
 
     return render(request, 'empresa/insert_update.html', {
         'mensaje': mensaje,
-        'form': form
+        'form': form,
+        'edit': edit,
     })
+
+
+def eliminar_empresa(request, id):
+    Empresa.objects.get(pk=id).delete()
+    return redirect('IndexEmpresa')
